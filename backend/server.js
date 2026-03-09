585aa2542102e446b02a8acdbd3db5d764b3d0cd
carlosscorreia <carloscorreia.tech@gmail.com>
2026-03-09 10:58:28 -0300

    feat: rota admin

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 require('./config/db');
 
 const userRoutes = require('./routes/userRoutes');
+const adminRoutes = require('./routes/adminRoutes');
 
 const app = express();
 
@@ -12,6 +13,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/users', userRoutes);
+app.use('/api/admin', adminRoutes);
 
 // Rota de teste
 app.get('/health', (req, res) => {
