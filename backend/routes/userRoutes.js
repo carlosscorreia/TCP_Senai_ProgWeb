d5dc5c40c96d387d516d32024437e4303c983583
carlosscorreia <carloscorreia.tech@gmail.com>
2026-03-09 12:41:00 -0300

    refactor: carinho e pedidos

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
+const { verificarUsuario } = require('../middleware/auth')
 
 router.post('/register', userController.register);
 router.post('/login', userController.login);
+router.get('/profile', verificarUsuario, userController.getProfile);
+
 
 module.exports = router;
