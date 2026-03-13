8c5dc08260d1072ba3687f78f8e6efd774066d77
carlosscorreia <carloscorreia.tech@gmail.com>
2026-03-13 18:04:51 -0300

    remove arquivos desnecessários

--- database/alteracoes.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE pedidos 
-ADD COLUMN modalidade ENUM('entrega','retirada') DEFAULT 'entrega',
-ADD COLUMN frete DECIMAL(10,2) DEFAULT 0.00,
-ADD COLUMN pagamento VARCHAR(50) NULL,
-ADD COLUMN troco DECIMAL(10,2) NULL;
